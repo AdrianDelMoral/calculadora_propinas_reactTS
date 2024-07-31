f171d9574cddbbf432a6403df1b7559b6a0d7374
Adrián Del Moral <adrian.delmoral.martin@gmail.com>
2024-08-01 01:13:38 +0200

    Creating the repository with the initial steps

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <header className="bg-cyan-400 py-4">
+        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
