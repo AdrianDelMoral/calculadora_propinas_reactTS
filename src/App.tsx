9860bfe4ce4d665647e8ecfd3cf703e71e9b783c
Adrián Del Moral <adrian.delmoral.martin@gmail.com>
2024-08-01 01:41:25 +0200

    Creating the component and listing the component on the Menus

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
+import { MenuItem } from "./components/MenuItem"
 import { menuItems } from "./data/db"
 
 function App() {
 
-  console.log(menuItems);  
-
   return (
     <>
       <header className="bg-cyan-400 py-4">
-        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumición</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2 className="text-center text-2xl font-semibold">Menú</h2>
+
+          {menuItems.map(item =>(
+            <MenuItem
+              key={item.id}
+            />
+          ))}
+        </div>
+        
+        <div>
+          <h2 className="text-center text-2xl font-semibold">Consumición</h2>
+
+        </div>
+      </main>
     </>
   )
 }
