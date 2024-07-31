6e63ab7c9ee479e84f036823402ff55fb9340888
Adrián Del Moral <adrian.delmoral.martin@gmail.com>
2024-08-01 01:25:09 +0200

    Adding Products and creating a test database linking with typed

---
+++ src/data/db.ts
@@ -0,0 +1,64 @@
+import type { MenuItem } from "../types"
+
+export const menuItems : MenuItem[] = [
+    {
+        "id": 1,
+        "name": "Pizza a la Leña Pequeña",
+        "price": 14
+    },
+    {
+        "id": 2,
+        "name": "Pizza a la Leña Mediana",
+        "price": 17
+    },
+    {
+        "id": 3,
+        "name": "Rebanada de Pan con Tomate",
+        "price": 5
+    },
+    {
+        "id": 4,
+        "name": "Rebanada de Pastel de Chocolate",
+        "price": 7
+    },
+    {
+        "id": 5,
+        "name": "Zumo de Naranja",
+        "price": 3
+    },
+    {
+        "id": 6,
+        "name": "Pizza a la Leña Grande",
+        "price": 20
+    },
+    {
+        "id": 7,
+        "name": "Chuleta de ternera 800g",
+        "price": 25
+    },
+    {
+        "id": 8,
+        "name": "Zumo de Limón",
+        "price": 3
+    },
+    {
+        "id": 9,
+        "name": "Tequila",
+        "price": 12
+    },
+    {
+        "id": 10,
+        "name": "Rebanada de Pan gratinado de Queso",
+        "price": 5
+    },
+    {
+        "id": 11,
+        "name": "Café Americano",
+        "price": 5
+    },
+    {
+        "id": 12,
+        "name": "Café Capuchino",
+        "price": 4
+    }
+]
