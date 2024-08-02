c99b178af4bc79d0d8037aeb1f3063103b6ff15b
Adrián Del Moral <adrian.delmoral.martin@gmail.com>
2024-08-02 12:19:26 +0200

    Adding reference to be able to display the menu item in MenuItem

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import type { OrderItem } from "../types";
+import type { MenuItem, OrderItem } from "../types";
 
 export default function useOrder(){
     const [order, setOrder] = useState<OrderItem[]>([])
@@ -7,8 +7,8 @@ export default function useOrder(){
        const [auth, setAuth] = useState<boolean>(false) // Es redundante si ya el valor inicial lo indica, se llama generic de boolean  si se lo indicamos
     */
 
-    const addItem = () => {
-        console.log('agregando...')
+    const addItem = (item : MenuItem) => {
+        console.log(item)
     }
 
     return {
